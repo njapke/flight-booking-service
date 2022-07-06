425a9124067fa1418b107fd80fef705ac9eacfe8
Christoph Witzko <github@christophwitzko.com>
2022-07-06 13:23:28 +0200

    feat: add booking model

--- pkg/database/models/booking.go
+++ pkg/database/models/booking.go
@@ -1,11 +1,17 @@
 package models
 
+type Passenger struct {
+	Name string `json:"name"`
+	Seat string `json:"seat"`
+}
+
 type Booking struct {
-	ID       string `json:"id"`
-	UserID   string `json:"userId"`
-	FlightID string `json:"flightId"`
-	Price    int    `json:"price"`
-	Status   string `json:"status"`
+	ID         string      `json:"id"`
+	UserID     string      `json:"userId"`
+	FlightID   string      `json:"flightId"`
+	Price      int         `json:"price"`
+	Status     string      `json:"status"`
+	Passengers []Passenger `json:"passengers"`
 }
 
 func (b *Booking) Collection() string {
