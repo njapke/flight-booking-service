41e3e42c95963877f32d485a8f2fb7d29efb5a74
Christoph Witzko <github@christophwitzko.com>
2022-07-11 18:08:00 +0200

    test: add basic artillery tests

---
+++ artillery/helper.js
@@ -0,0 +1,34 @@
+function setRandomFlightId (context, events, done) {
+  context.vars.flightId = context.vars.flights[Math.floor(Math.random() * context.vars.flights.length)].id
+  return done()
+}
+
+function setBookingRequest (context, events, done) {
+  const pickedSeats = []
+  function pickRandomSeat () {
+    for (let i = 0; i < 10; i++) {
+      const seat = context.vars.seats[Math.floor(Math.random() * context.vars.seats.length)].seat
+      if (!pickedSeats.includes(seat)) {
+        pickedSeats.push(seat)
+        return seat
+      }
+    }
+    throw new Error('Could not find an available seat')
+  }
+
+  const passengers = Array.from(Array(2))
+    .map((_v, i) => ({
+      name: `Passenger ${i}`,
+      seat: pickRandomSeat()
+    }))
+  context.vars.bookingRequest = {
+    flightId: context.vars.flightId,
+    passengers
+  }
+  return done()
+}
+
+module.exports = {
+  setRandomFlightId,
+  setBookingRequest
+}
