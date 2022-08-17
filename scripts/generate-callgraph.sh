035e8d016021aeda4d5382e0390b18f4d3ee8049
Christoph Witzko <github@christophwitzko.com>
2022-08-17 12:58:06 +0200

    chore(callgraph): use dot output format

--- scripts/generate-callgraph.sh
+++ scripts/generate-callgraph.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # using https://pkg.go.dev/golang.org/x/tools@v0.1.11/cmd/callgraph
 
-callgraph -algo pta -format digraph ./cmd/flight-booking-service > callgraph.txt
+callgraph -algo pta -format graphviz ./cmd/flight-booking-service > static-callgraph.dot
