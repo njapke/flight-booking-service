3b81126f7741b39f4bb8d31b054cd641e092233f
Christoph Witzko <github@christophwitzko.com>
2022-08-24 16:10:47 +0200

    feat(artillery): add more metrics

--- artillery/helper.js
+++ artillery/helper.js
@@ -1,3 +1,5 @@
+const url = require('url')
+
 function setRandomFlightId (context, events, done) {
   context.vars.flightId = context.vars.flights[Math.floor(Math.random() * context.vars.flights.length)].id
   return done()
@@ -45,8 +47,26 @@ function setRandomDestination (context, events, done) {
   return done()
 }
 
+// adapted from https://github.com/artilleryio/artillery-plugin-metrics-by-endpoint/blob/master/index.js
+function afterResponse (requestParams, response, context, ee, next) {
+  let basePath = (new url.URL(requestParams.url)).pathname
+  if (basePath.endsWith('/seats')) {
+    // remove flight id from request path
+    const pathEl = basePath.split('/')
+    pathEl[2] = '$flightID'
+    basePath = pathEl.join('/')
+  }
+  const metricName = `scenario.${requestParams.scenarioName}.${requestParams.method}.${basePath}`
+  ee.emit('counter', `${metricName}.response.${response.statusCode}`, 1)
+  ee.emit('histogram', `${metricName}.total`, response.timings.phases.total)
+  // ee.emit('histogram', `${metricName}.firstByte`, response.timings.phases.firstByte)
+  // console.log(metricName, response.timings.phases)
+  return next()
+}
+
 module.exports = {
   setRandomFlightId,
   setBookingRequest,
-  setRandomDestination
+  setRandomDestination,
+  afterResponse
 }
